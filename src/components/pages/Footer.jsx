fbaf2b4c19a9e9102c03c97dda82dc1a2b290d0e
Ghiftee <gigibetine@gmail.com>
2023-01-09 23:01:17 +0100

    Add social media icons

--- src/components/pages/Footer.jsx
+++ src/components/pages/Footer.jsx
@@ -1,13 +1,15 @@
+import { FaTwitter, FaFacebookF, FaInstagramSquare } from 'react-icons/fa';
+
 function Footer() {
   return (
       
     <footer className="footer">
-      <p className="footer-p">&copy;Team Hypertext </p>
       <ul className="footer-list">
-        <li>Twitter</li>
-        <li>Facebook</li>
-        <li>Instagram</li>
+        <li><FaTwitter /></li>
+        <li><FaFacebookF/></li>
+        <li><FaInstagramSquare/></li>
       </ul>
+      <p className="footer-p">&copy;Team Hypertext </p>
     </footer>
      
   )
